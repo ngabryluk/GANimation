7a1c9a05ede14170de91911f738aab9371f2c18e
ngabryluk <ncgabryluk@gmail.com>
2023-11-30 02:08:17 -0500

    Loads in data properly
    Takes every other frame and saves them as pairs with the middle frame being saved separately to be used as ground truth

---
+++ test2.py
@@ -0,0 +1,73 @@
+import numpy as np
+import os
+import imageio.v2
+import pdb
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+NUM_VIDEOS = 500 # Total number of "videos" or sets of frames that were generated
+NUM_FRAMES = 50 # Number of frames per video
+
+# Idea to try: Use half of the frames and interpolate the frames in between to get back to 50, but save those in between frames to be the ground truth
+def load():
+    DATA_PATH = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), "Data\\dataset"
+    )
+    SAVE_PATH = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), "Data\\save_folder"
+        )
+    if not os.path.exists(SAVE_PATH):
+        os.makedirs(SAVE_PATH)
+        # Reads in the images from our synthetic dataset into a numpy array
+        images = os.listdir(DATA_PATH)
+        # Iterate through every other frame and save it to 
+        frame_pairs = np.empty((((NUM_FRAMES // 2) - 1) * NUM_VIDEOS, 256, 512)) # (12000, 256, 512) 24 pairs per 500 samples
+        real_middles = np.empty((((NUM_FRAMES // 2) - 1) * NUM_VIDEOS, 256, 256)) # (12000, 256, 512) 24 middle frames per 500 samples
+        pairs_index, middles_index = 0, 0
+        for i in range(0, len(images) - 2, 2):
+            # If we are at the second to last frame, continue because the frame 2 spots ahead is the next sample
+            if (i + 2) % NUM_FRAMES == 0:
+                continue
+            
+            filepath1 = os.path.join(DATA_PATH, images[i])
+            filepath2 = os.path.join(DATA_PATH, images[i + 2])
+            filepath3 = os.path.join(DATA_PATH, images[i + 1])
+            
+            arr1 = np.array(imageio.imread(filepath1))
+            arr2 = np.array(imageio.imread(filepath2))
+            arr3 = np.array(imageio.imread(filepath3))
+
+            arr1 = np.dot(arr1[..., :3], [0.2989, 0.5870, 0.1140])
+            arr2 = np.dot(arr2[..., :3], [0.2989, 0.5870, 0.1140])
+            arr3 = np.dot(arr3[..., :3], [0.2989, 0.5870, 0.1140])
+
+            arr1 = np.round(arr1, 0)
+            arr2 = np.round(arr2, 0)
+            arr3 = np.round(arr3, 0)
+
+            frame_pairs[pairs_index] = np.hstack((arr1, arr2))
+            pairs_index += 1
+            
+            real_middles[middles_index] = arr3
+            middles_index += 1
+
+        np.save(os.path.join(SAVE_PATH, "frame_pairs.npy"), frame_pairs)
+        np.save(os.path.join(SAVE_PATH, "real_middles.npy"), real_middles)
+    frame_pairs = np.load(os.path.join(SAVE_PATH, "frame_pairs.npy"))
+    real_middles = np.load(os.path.join(SAVE_PATH, "real_middles.npy"))
+    return frame_pairs, real_middles
+
+
+def to_gif(images):
+    converted_images = np.clip(images, 0, 255).astype(np.uint8)
+    imageio.mimsave("./animation.gif", converted_images, fps=25)
+    # return embed.embed_file("./animation.gif")
+
+
+frame_pairs, real_middles = load()
+
+# INP = input_images[0].reshape(256, 512, 1)
+
+OUTPUT_CHANNELS = 3
+
+pdb.set_trace()
