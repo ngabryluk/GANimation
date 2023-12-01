ee0a3a4956092ffd42ba7ca3899afd4f7984e5c7
ngabryluk <ncgabryluk@gmail.com>
2023-12-01 14:39:52 -0500

    Fixed some errors

--- test2.py
+++ test2.py
@@ -73,42 +73,87 @@ def to_gif(images):
 
 frame_pairs, real_middles = load()
 
-pdb.set_trace()
+# pdb.set_trace()
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    
-    # Downsample a little then upsample
-    model.add(layers.Reshape((256, 256, 1), input_shape=(256, 256)))
+def downsample(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 16, 16, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                                kernel_initializer=initializer, use_bias=False))
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 32, 32, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 64, 64, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    result.add(tf.keras.layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 128, 128, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    return result
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 256, 256, 1)
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
 
-    return model
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                        padding='same',
+                                        kernel_initializer=initializer,
+                                        use_bias=False))
 
-generator = make_generator_model()
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+
+def Generator():
+    inputs = tf.keras.layers.Input(shape=[256, 256, 1])
+
+    down_stack = [
+        downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
+        downsample(128, 4),  # (batch_size, 64, 64, 128)
+        downsample(256, 4),  # (batch_size, 32, 32, 256)
+        downsample(512, 4),  # (batch_size, 16, 16, 512)
+    ]
+
+    up_stack = [
+        upsample(256, 4),  # (batch_size, 32, 32, 512)
+        upsample(128, 4),  # (batch_size, 64, 64, 256)
+        upsample(64, 4),  # (batch_size, 128, 128, 128)
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(1, 4,
+                                            strides=2,
+                                            padding='same',
+                                            kernel_initializer=initializer,
+                                            activation='tanh')  # (batch_size, 256, 256, 1)
+
+    x = inputs
+
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = tf.keras.layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    return tf.keras.Model(inputs=inputs, outputs=x)
+
+generator = Generator()
 generated_image = generator(frame_pairs[0, :, :, :256], training=False)
-pdb.set_trace()
+# pdb.set_trace()
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
@@ -128,7 +173,7 @@ def make_discriminator_model():
 
 discriminator = make_discriminator_model()
 decision = discriminator(generated_image)
-print (decision)
+# print (decision)
 
 # This method returns a helper function to compute cross entropy loss
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -139,28 +184,33 @@ def discriminator_loss(real_output, fake_output):
     total_loss = real_loss + fake_loss
     return total_loss
 
-def generator_loss(fake_output):
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
+def generator_loss(fake_output, real_middles):
+    real_middles = tf.cast(real_middles, dtype=tf.float32)
+
+    # pdb.set_trace()
+    
+    # Calculate the mean absolute error (MAE) between generated frames and ground truth middles
+    mae = tf.reduce_mean(tf.abs(fake_output[:, :, :, 0] - real_middles))
+    return mae
 
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 EPOCHS = 10
 noise_dim = 100
-num_examples_to_generate = 16
 
 # Notice the use of `tf.function`
 # This annotation causes the function to be "compiled".
 @tf.function
 def train_step(frame_pairs, real_middles):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        current_frames = frame_pairs[:, :, :, :, :256]
+        current_frames = frame_pairs[:, :, :256]
         generated_images = generator(current_frames, training=True)
         
         real_output = discriminator(real_middles, training=True)
         fake_output = discriminator(generated_images, training=True)
 
-        gen_loss = generator_loss(fake_output, real_middles)
+        gen_loss = generator_loss(generated_images, real_middles)
         disc_loss = discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
@@ -172,7 +222,7 @@ def train_step(frame_pairs, real_middles):
 def train(frame_pairs, real_middles, epochs):
     for epoch in range(epochs):
         start = time.time()
-        # pdb.set_trace()
+        pdb.set_trace()
         for i in range(len(frame_pairs)):
             current_frames = frame_pairs[i]
             ground_truth_middles = real_middles[i]
@@ -181,12 +231,12 @@ def train(frame_pairs, real_middles, epochs):
             train_step(current_frames, ground_truth_middles)
 
         # Produce images for the GIF as you go
-        generate_and_save_images(generator, epoch + 1, frame_pairs[:, 0, :, :, :256])
+        generate_and_save_images(generator, epoch + 1, frame_pairs[0, :, :, :256])
 
         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
     # Generate after the final epoch
-    generate_and_save_images(generator, epochs, frame_pairs[:, 0, :, :, :256])
+    generate_and_save_images(generator, epochs, frame_pairs[0, :, :, :256])
     
 def generate_and_save_images(model, epoch, test_input):
     # Notice `training` is set to False.
